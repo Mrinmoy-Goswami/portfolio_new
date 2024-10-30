6906846cc824b92f93e585e8fdc81527d17897ff
Mrinmoy Goswami <you@example.com>
2024-10-30 11:29:04 +0530

    image compression

--- src/components/uiComp/ProjectCard.jsx
+++ src/components/uiComp/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({title,description,image,link}) => {
     
     <div className='sm:w-1/4 w-3/4   h-[20rem] md:h-[18rem] backdrop-blur-lg  rounded shadow-md shadow-white
     transition ease-in-out delay-150 bg-blue-500 m-5 p-4  font-sans flex  flex-col justify-center items-center bg-gradient-to-r from-gray-600 to-gray-800'>
-      <img src={image} className='h-1/2 w-full object-cover bg-blend-color-burn rounded'  alt='image' loading='lazy'/>
+      <img src={image} className='h-1/2 w-full object-cover bg-blend-color-burn rounded'  alt='image' />
      
       <span className='sm:text-xl text-sm font-bold text-gray-300'>{title}</span>
       <span className='text-sm text-start text-gray-300 w-full overflow-auto'>{description}</span>
